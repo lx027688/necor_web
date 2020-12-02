2f83f8d54cd820267d3736a257c3d5a0a3127a4b
lixiang <565180713@qq.com>
2020-12-02 16:26:23 +0800

    修改接口信息模块

--- src/views/system/interface_info/index.vue
+++ src/views/system/interface_info/index.vue
@@ -1,15 +1,12 @@
 <template>
 <d2-container>
 	<!-- 查询 -->
-	<el-form :inline="true" :model="query" ref="form" style="margin-bottom: -18px;">
-		<el-form-item label="" prop="search">
-			<el-input v-model="query.search" placeholder="搜索项" style="width: 180px;"/>
-		</el-form-item>
-    <el-form-item label="" prop="search">
-      <necor-dict-select v-model="query.isLog" code="100" placeholder="是否开启接口日志"></necor-dict-select>
+	<el-form :inline="true" ref="form" style="margin-bottom: -18px;">
+    <el-form-item label="" prop="keywords">
+      <el-input v-model="keywords" placeholder="搜索项" style="width: 180px;"/>
     </el-form-item>
 		<el-form-item>
-      <el-button type="primary" @click="search">
+      <el-button type="primary" @click="query">
         <d2-icon name="search"/> 查询
       </el-button>
     </el-form-item>
@@ -26,11 +23,9 @@
 	</el-form>
 
 	<!-- 列表-->
-	<el-table :data="data" @sort-change="sortChange" @selection-change="selectionChangeHandle" v-loading="loading" stripe border style="width: 100%;margin-top: 10px;margin-bottom: 20px;">
+	<el-table :data="showData.slice((page.currentPage-1)*page.pageSize,page.currentPage*page.pageSize)" @selection-change="selectionChangeHandle" v-loading="loading" stripe border style="width: 100%;margin-top: 10px;margin-bottom: 20px;">
 		<el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
-<!--		<el-table-column prop="id" header-align="center" align="center" sortable="custom" label="ID"></el-table-column>-->
-<!--		<el-table-column prop="createDate" header-align="center" align="center" sortable="custom" label="创建时间"></el-table-column>-->
-<!--		<el-table-column prop="updateDate" header-align="center" align="center" sortable="custom" label="修改时间"></el-table-column>-->
+    <!--<el-table-column prop="id" header-align="center" align="center" sortable="custom" label="ID"></el-table-column>-->
     <el-table-column prop="className" header-align="center" align="center" label="接口类名" width="550"></el-table-column>
 		<el-table-column prop="methodName" header-align="center" align="center" label="接口方法名" width="360"></el-table-column>
 		<el-table-column prop="interfaceUrl" header-align="center" align="center" label="接口地址" width="350"></el-table-column>
@@ -40,39 +35,32 @@
         <el-tag :type="scope.row.isLog==='100000' ? 'success' : 'danger'" @click="updateInfIsLog(scope.row.id,scope.row.isLog)" disable-transitions>{{scope.row.isLog==='100000'?'需要记录日志':'无需记录日志'}}</el-tag>
       </template>
     </el-table-column>
-	<!--	<el-table-column fixed="right" header-align="center" align="center" width="180" label="操作">
-			<template slot-scope="scope">
-				<el-button type="text" size="small">查看</el-button>
-			</template>
-		</el-table-column>-->
 	</el-table>
 
 	<!-- 列表尾部-->
-	<pagination :cp.sync="query.currentPage" :ps.sync="query.pageSize" :total.sync="query.total" @pagination="getList"></pagination>
-
+  <el-pagination layout="sizes, prev, pager, next" :current-page="page.currentPage" :page-size="page.pageSize"
+                 :page-sizes="page.pageSizes" :total="showData.length" @size-change="handleSizeChange" @current-change="handleCurrentChange">
+  </el-pagination>
 </d2-container>
 </template>
 
 <script>
 import { list, updateIsLog, batchUpdateIsLog } from '@api/system/interfaceInfo'
-import pagination from '@/components/pagination'
 
 export default {
 	name: 'system-interfaceInfo',
-	components: { pagination },
 	data() {
 		return {
+      keywords: '',
 			loading: false,
-			query: {
+			page: {
 				currentPage: 1,
-				pageSize: 10,
-				total: 0,
-				search: '',
-				isLog: '',
-				orderKey: '',
-				orderVal: ''
-			},
+        pageSize: 10,
+        total: 0,
+        pageSizes:[10,20,50,100]
+      },
 			data: [],
+			showData: [],
       ids: []
 		}
 	},
@@ -80,29 +68,32 @@ export default {
 		this.getList()
 	},
 	methods: {
-		search () {
-			this.query.currentPage = 1
-			this.getList()
-		},
 		getList () {
 			this.loading = true
-			list({...this.query}).then(r => {
-        let res = r.data
+			list().then(res => {
         this.data = res.data
-        this.query.total = res.recordsFiltered
+        this.showData = this.data
         this.loading = false
 			}).catch(err => {
 				console.log('err', err)
 				this.loading = false
 			})
 		},
-		sortChange(column, prop, order) {
-			this.query.orderKey = column.prop
-			this.query.orderVal = column.order
-			if (this.query.orderKey !== undefined && this.query.orderVal !== undefined) {
-				this.getList()
-			}
-		},
+    query () {
+      this.showData = this.data.filter(item => {
+        return item.className.match(this.keywords) || item.methodName.match(this.keywords) || item.interfaceUrl.match(this.keywords)
+      })
+
+      this.page.currentPage = 1
+    },
+    // 翻页方法
+    handleSizeChange(val) {
+      this.page.currentPage = 1;
+      this.page.pageSize = val;
+    },
+    handleCurrentChange(val) {
+      this.page.currentPage = val;
+    },
     selectionChangeHandle: function (val) {
       this.ids = val.map(o => {return o.id})
     },
