3b441c60c87bec571efa0b447f719e21ae63aa3d
lixiang <565180713@qq.com>
2025-08-21 11:18:03 +0800

    refactor(sys): 移除区域保存表单中邮编字段的非空验证
    - 在 save.vue 文件中的 saveRule 规则中，移除了 zipCode 字段的 min 验证规则
    -此修改旨在允许邮编字段可以为空，提高了表单的灵活性和用户体验

--- src/views/sys/area/save.vue
+++ src/views/sys/area/save.vue
@@ -62,7 +62,7 @@ export default {
       saveRule: {
         name: [{ required: true, message: '请输入区域名称', trigger: 'blur' }, { min: 0, max: 40, message: '长度不能超过40个字符', trigger: 'blur' }],
         code: [{ required: true, message: '请输入区域代码', trigger: 'blur' }, { min: 0, max: 15, message: '长度不能超过15个字符', trigger: 'blur' }],
-        zipCode: [{ min: 0, max: 10, message: '长度不能超过10个字符', trigger: 'blur' }],
+        zipCode: [{ max: 10, message: '长度不能超过10个字符', trigger: 'blur' }],
         lon: [{ required: true, message: '请输入区域经度', trigger: 'blur' }, { min: 0, max: 30, message: '长度不能超过30个字符', trigger: 'blur' }],
         lat: [{ required: true, message: '请输入区域纬度', trigger: 'blur' }, { min: 0, max: 30, message: '长度不能超过30个字符', trigger: 'blur' }],
         simpleName: [{ required: true, message: '请输入区域简称', trigger: 'blur' }, { min: 0, max: 40, message: '长度不能超过40个字符', trigger: 'blur' }],
